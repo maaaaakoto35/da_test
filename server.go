dad1f53c8c2b0c18f0c644bde797502df6b758c1
fujiwara.makoto <fujiwara.makoto@Donuts-N412.local>
2021-04-22 18:08:37 +0900

    [develop] adds api path

--- server.go
+++ server.go
@@ -13,5 +13,22 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+	e.GET("/v1/number2kanji/:param", func(c echo.Context) error { return num2kanjiHandler(c) })
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func num2kanjiHandler(c echo.Context) (err error) {
+	param := c.Param("param")
+	res, err := Kanji2number(param)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, struct {
+			Message string
+		}{
+			Message: "変換できません.",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, res)
+	return
 }
